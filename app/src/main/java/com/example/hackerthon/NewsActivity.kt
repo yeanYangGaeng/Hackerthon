b278eb9be54d1bcf71876bfe21eff955a7a56df1
JeongeunChoi <cju4103@gmail.com>
2021-10-30 02:15:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/hackerthon/NewsActivity.kt
@@ -0,0 +1,37 @@
+package com.example.hackerthon
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+        val settingButton: ImageButton = findViewById(R.id.settingButton)
+        settingButton.setOnClickListener{
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
+        val newsButton: ImageButton = findViewById(R.id.newsButton)
+        newsButton.setOnClickListener{
+            val intent = Intent(this, NewsActivity::class.java)
+            startActivity(intent)
+        }
+        val homeButton: ImageButton = findViewById(R.id.homeButton)
+        homeButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        val searchingButton: ImageButton = findViewById(R.id.searchingButton)
+        searchingButton.setOnClickListener{
+            val intent = Intent(this, SearchingActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
